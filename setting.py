570cdcdc5a6ccb366ba4805b6221a5b0da7b9434
zkh-dot <kernzahar@gmail.com>
2023-09-18 18:58:33 +0300

    add re expression to setting

--- setting.py
+++ setting.py
@@ -2,3 +2,4 @@ token = ""
 page_error = "Ошибка: странная ссылка"
 success_add_message = "Добавил сайт"
 ask_for_help_message = "С этой страницей какая-то проблема, попробуйте еще раз или обратитесь к @Lunitarik за поддержкой"
+reg_expression = r'.*?href=\"(.*?)\".*?title=\"(.*?)\"'

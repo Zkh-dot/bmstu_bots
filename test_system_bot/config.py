4b143e9dedbf110aeb15e727c88028d4929b2d13
zkh-dot <kernzahar@gmail.com>
2023-10-01 14:57:18 +0300

    add constructor

--- test_system_bot/config.py
+++ test_system_bot/config.py
@@ -1,1 +1,29 @@
 token=""
+
+# get users file mesages
+saved_text = "Сохранил ваш тест!"
+example_text = "Пример документа:"
+
+# pass test messages
+start_test = "Начинаем тест!"
+test_readfile_error = "С этим тестом что-то не так"
+test_expired_error = f"Эта экспертная система устарела! Обратитесь к автору-эксперту для ее обновления."
+test_wrongresult_error = "Автор теста ебланчик :С"
+test_result = "Тест завершен! Ваш результат: \n"
+notexistingresult_error = "Странная ошибка в системе. Обраитесь к эксперту."
+
+# create test in constructor messages
+end_test_command = "/test_is_done"
+end_question_command = "/question_is_done"
+end_options_command = "/option_is_done"
+abort_creation_command = "/break"
+start_test_creation = f"Сейчас вы начнете создание своей экспертной системы. Чтобы прервать создание экспертной системы и отчистить все результаты, введите команду {abort_creation_command}"
+
+ask_for_question = f"Пожалуйста, напишите вопрос. Если вы ввели все вопросы, введите команду {end_test_command}"
+asf_for_option = f"Пожалуйста, напишите вариант ответа. Если вы ввели все варианты, введите команду {end_question_command}"
+ask_for_option_param = f"Пожалуйста, напишите, какие параметры увеличатся при выборе этого варианта. В случае, если вы ввели все параметры, введите команду {end_options_command}"
+
+end_test_creation = "Ваша экспертная система успешно сохранена! Введите команду /list чтобы посмотреть на нее."
+not_valid_test_creation_error = "Вы ввели что-то не так. К сожалению, на данный момент система не поддерживает редактирование, поэтому вам придется создать систему заново."
+no_username_error = "К сожалению, люди без юзернейма ограничены в правах и не могут создавать тесты. Вы можете поплакать об этом."
+creation_end = "Создание теста завершено."
--- test_system_bot/lab_bot.py
+++ test_system_bot/lab_bot.py
@@ -11,18 +11,77 @@ def add_test(message):
     downloaded_file = bot.download_file(file_info.file_path)
     with open(os.path.join("tests", message.chat.username), 'wb') as new_file:
         new_file.write(downloaded_file)
-    bot.send_message(message.chat.id, "Сохранил ваш тест!")
+    bot.send_message(message.chat.id, config.saved_text)
+
 
 @bot.message_handler(commands=["list"])
 def list_all(message):
     bot.send_message(message.chat.id, "/" + "\n/".join(os.listdir("./tests")))
 
+
 @bot.message_handler(commands=["help"])
 def list_all(message):
-    bot.send_message(message.chat.id, "Пример документа:")
-    with open(os.path.join("tests", "Lunitarik"), "rb") as file:
+    bot.send_message(message.chat.id, config.example_text)
+    with open(os.path.join("tests", "example"), "rb") as file:
         bot.send_document(message.chat.id, document=file)
 
+
+@bot.message_handler(commands=["create"])
+def start_test_creation(message):
+    bot.send_message(message.chat.id, config.start_test_creation)
+    bot.send_message(message.chat.id, config.ask_for_question)
+    test = {
+        "questions": [],
+        "test_results": {}
+    }
+    bot.register_next_step_handler(message, lambda m: get_test_question(m, test))
+
+
+def get_test_question(message, test):
+    if message.text == config.abort_creation_command:
+        bot.send_message(message.chat.id, config.creation_end)
+        del test
+    elif message.text == config.end_test_command:
+        evaluate_test(message, test)     
+    else:
+        test["questions"].append({message.text: {}})    
+        bot.send_message(message.chat.id, config.ask_for_option_param)
+        bot.register_next_step_handler(message, lambda m: get_test_option(m, test))
+
+
+def get_test_option(message, test):
+    if message.text == config.abort_creation_command:
+        bot.send_message(message.chat.id, config.creation_end)
+    elif message.text == config.end_test_command:
+        evaluate_test(message, test)     
+    elif message.text == config.end_question_command:
+        bot.send_message(message.chat.id, config.ask_for_question)
+        bot.register_next_step_handler(message, lambda m: get_test_question(m, test))
+    else:
+        test["questions"][-1][list(test["questions"][-1].keys())[0]][message.text] = []
+        bot.send_message(message.chat.id, config.ask_for_option_param)
+        bot.register_next_step_handler(message, lambda m: get_option_params(m, test))
+
+def get_option_params(message, test):
+    if message.text == config.abort_creation_command:
+        bot.send_message(message.chat.id, config.creation_end)
+    elif message.text == config.end_test_command:
+        evaluate_test(message, test)     
+    elif message.text == config.end_question_command:
+        bot.send_message(message.chat.id, config.ask_for_question)
+        bot.register_next_step_handler(message, lambda m: get_test_question(m, test))
+    elif message.text == config.end_options_command:
+        bot.send_message(message.chat.id, config.asf_for_option)
+        bot.register_next_step_handler(message, lambda m: get_test_option(m, test))
+    else:
+        test["questions"][-1][list(test["questions"][-1].keys())[0]][message.text].append(message.text)
+        bot.send_message(message.chat.id, config.ask_for_option_param)
+        bot.register_next_step_handler(message, lambda m: get_option_params(m, test))
+
+
+def evaluate_test(message, test):
+    # TODO: check
+    bot.send_message(message.chat.id, config.end_test_creation)
 
 @bot.message_handler(content_types=["text"])
 def start_test(message):
@@ -30,9 +89,9 @@ def start_test(message):
         with open(os.path.join(".\\tests", message.text[1:]), encoding="utf-8") as test_file:
             test = json.load(test_file)
     except:
-        bot.send_message(message.chat.id, "С этим тестом что-то не так")
+        bot.send_message(message.chat.id, config.test_readfile_error)
     else:
-        bot.send_message(message.chat.id, "Начинаем тест!")
+        bot.send_message(message.chat.id, config.start_test)
     answers = test["results"].copy()
     for a in answers:
         answers[a] = 0
@@ -49,13 +108,13 @@ def test_step(message, i, test, answers):
                 answers[option] += 1
                 print(answers)
             except KeyError:
-                bot.send_message(message.chat.id, "Автор теста ебланчик :С")
+                bot.send_message(message.chat.id, config.test_wrongresult_error)
     if i >= len(test["questions"]):
         # validate(message, test, answers)
         try:
-            validate(message, test, answers)
+            test_results(message, test, answers)
         except Exception as e:
-            bot.send_message(message.chat.id, f"Эта экспертная система устарела! Обратитесь к автору-эксперту для ее обновления.\n{e}")
+            bot.send_message(message.chat.id, config.test_expired_error)
         # bot.send_message(message.chat.id, f"Тест завершен! Ваш результат: \n{test['results'][max(answers, key=answers.get)]}", reply_markup=telebot.types.ReplyKeyboardRemove())
     
     else:
@@ -69,7 +128,7 @@ def test_step(message, i, test, answers):
         bot.register_next_step_handler(message, lambda m: test_step(m, i + 1, test, answers))
 
 
-def validate(message, test, answers):
+def test_results(message, test, answers):
     print(answers)
     print(test["test_results"])
     for result in test["test_results"]:
@@ -79,10 +138,9 @@ def validate(message, test, answers):
             if answers[option] < test["test_results"][result][option][0] or answers[option] > test["test_results"][result][option][1]:
                 break
         else:
-            bot.send_message(message.chat.id, f"Тест завершен! Ваш результат: \n{result}", reply_markup=telebot.types.ReplyKeyboardRemove())
+            bot.send_message(message.chat.id, config.test_result + result, reply_markup=telebot.types.ReplyKeyboardRemove())
             return
-    bot.send_message(message.chat.id, "Странная ошибка в системе. Обраитесь к эксперту.", reply_markup=telebot.types.ReplyKeyboardRemove())
-
+    bot.send_message(message.chat.id, config.notexistingresult_error, reply_markup=telebot.types.ReplyKeyboardRemove())
                 
             
 
